4fb9c1dae836a570267be71268b4aca7c0cda9e5
Firat-Gulec <firat.gulec@hotmail.com>
2022-10-15 12:03:27 +0300

    work

--- lib/onboard/view/onboarding_page.dart
+++ lib/onboard/view/onboarding_page.dart
@@ -1,15 +1,171 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:rxs_onboard_fg/onboard/view/welcome_view.dart';
 
-class Onboarding_View extends StatefulWidget {
+class Onboarding_View extends StatelessWidget {
   const Onboarding_View({super.key});
 
   @override
-  State<Onboarding_View> createState() => _Onboarding_ViewState();
-}
-
-class _Onboarding_ViewState extends State<Onboarding_View> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color kDarkBlueColor = Color(0xFF053149);
+
+
+    return OnBoardingSlider(
+      finishButtonText: 'Register',
+      onFinish: () {
+        Navigator.push(
+          context,
+          CupertinoPageRoute(
+            builder: (context) => WelcomeView(),
+          ),
+        );
+      },
+      finishButtonColor: kDarkBlueColor,
+      skipTextButton: Text(
+        'Skip',
+        style: TextStyle(
+          fontSize: 16,
+          color: kDarkBlueColor,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      trailing: Text(
+        'Login',
+        style: TextStyle(
+          fontSize: 16,
+          color: kDarkBlueColor,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      trailingFunction: () {
+        Navigator.push(
+          context,
+          CupertinoPageRoute(
+            builder: (context) => WelcomeView(),
+          ),
+        );
+      },
+      controllerColor: kDarkBlueColor,
+      totalPage: 3,
+      headerBackgroundColor: Colors.white,
+      pageBackgroundColor: Colors.white,
+      background: [
+        Image.asset(
+          'assets/images/slide_1.png',
+          height: 400,
+        ),
+        Image.asset(
+          'assets/images/slide_2.png',
+          height: 400,
+        ),
+        Image.asset(
+          'assets/images/slide_3.png',
+          height: 400,
+        ),
+      ],
+      speed: 1.8,
+      pageBodies: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 480,
+              ),
+              Text(
+                'On your way...',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: kDarkBlueColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'to find the perfect looking Onboarding for your app?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 480,
+              ),
+              Text(
+                'You’ve reached your destination.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: kDarkBlueColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Sliding with animation',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 480,
+              ),
+              Text(
+                'Start now!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: kDarkBlueColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Where everything is possible and customize your onboarding.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
