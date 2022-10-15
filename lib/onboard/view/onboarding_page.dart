fa4d926709f754093ac72b0c12e74e1285efab93
Firat-Gulec <firat.gulec@hotmail.com>
2022-10-15 11:05:14 +0300

    start

---
+++ lib/onboard/view/onboarding_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Onboarding_View extends StatefulWidget {
+  const Onboarding_View({super.key});
+
+  @override
+  State<Onboarding_View> createState() => _Onboarding_ViewState();
+}
+
+class _Onboarding_ViewState extends State<Onboarding_View> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
